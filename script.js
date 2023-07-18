c63f155ee9585c0f6383e3e2f29073df1839c505
Pâmela Amorim <pamelaamorim8@hotmail.com>
2023-07-18 16:51:04 -0300

    atualização

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function myFunction(getElementById) {
-    var dots = document.getElementById("mais" + getElementById);
-    var moreText = document.getElementById("id" + getElementById);
-    var btnText = document.getElementById("Btn" + getElementById);
+// function myFunction(getElementById) {
+//     var dots = document.getElementById("mais" + getElementById);
+//     var moreText = document.getElementById("id" + getElementById);
+//     var btnText = document.getElementById("Btn" + getElementById);
   
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Leia mais";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Leia menos";
-      moreText.style.display = "inline";
-    }
-  }
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Leia mais";
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Leia menos";
+//       moreText.style.display = "inline";
+//     }
+//   }
